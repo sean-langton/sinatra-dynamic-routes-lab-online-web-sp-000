85784cd5df327ab276d8214f2091e18829660336
Sean Langton <slangton1@gmail.com>
2019-01-29 23:22:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,4 +18,12 @@ class App < Sinatra::Base
     @phrase * @number
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1].to_s
+    @word2 = params[:word2].to_s
+    @word3 = params[:word3].to_s
+    @word4 = params[:word4].to_s
+    @word5 = params[:word5].to_s
+  end
+
 end
