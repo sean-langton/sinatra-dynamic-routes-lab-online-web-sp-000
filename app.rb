bd26ecf1f49f32aaa0b1658006486ef91e1bd19e
Sean Langton <slangton1@gmail.com>
2019-01-29 23:10:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,6 @@ class App < Sinatra::Base
   get '/say/:number/:phase' do
     @number = params[:number]
     @phrase = params[:phrase]
-    puts @phrase.times.@number
+    puts @phrase.times.@number.to_i
   end
 end
