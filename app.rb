470c027f48780304362c47114580f9cbd90c1f32
Sean Langton <slangton1@gmail.com>
2019-01-29 23:20:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase].to_s
-    @number.times do @phrase end
+    @number.times do "#{@phrase}" end
   end
 
 end
